d4964db60c4278bafee0f93a5c2c8b6c8263ff4a
Nabaneet Mondal <mondalnabaneet2@gmail.com>
2021-08-23 00:37:30 +0530

    Select2 Js Fixed, Working with Google Map API

--- getCity.php
+++ getCity.php
@@ -1,25 +1,15 @@
 <?php
-    $con = mysqli_connect("localhost", "root", "", "germin");
-    if(isset($_GET['q'])){
-        $q = $_GET['q'];
-        $stmt = $conn->prepare("SELECT * FROM city WHERE name LIKE ?");
-        $param = "%$q%";
-        $stmt->bind_param("ss", $param, $param);
-        $data = array();
-        if($stmt->execute()){
-            $result = $stmt->get_result();
-            if($result->$num_rows>0){
-                while($row = $result->fetch_assoc()){
-                    $id = $row['id'];
-                    $city = $row['name'];
-                    $data[] = array('id'=>$id, 'text'=>$city);
-                }
-                $stmt->close();
-            }
-            else{
-                $data[] = array('id'=>0, 'text'=>'No city found.');
-            }
-            echo json_encode($data);
-        }
-    }
-?>
+include 'config.php';
+
+if(!isset($_POST['searchTerm'])){ 
+  $fetchData = mysqli_query($con,"select * from india_map_excel order by city limit 5");
+}else{ 
+  $search = $_POST['searchTerm'];   
+  $fetchData = mysqli_query($con,"select * from india_map_excel where city like '%".$search."%' limit 5");
+} 
+
+$data = array();
+while ($row = mysqli_fetch_array($fetchData)) {    
+  $data[] = array("id"=>$row['id'], "text"=>$row['city']);
+}
+echo json_encode($data);
